ba8fd9521cc4d78950bb80148f3c14c4d5d3617f
agrandi1992 <74638083+agrandi1992@users.noreply.github.com>
2023-01-05 17:59:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,13 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
 import pandas as pd
 
 import io
 
 buffer = io.BytesIO()
-c=pd.read_excel("C.xlsx")
-ba=pd.read_excel("b.xlsx")
-b=c.columns
-l=ba.columns
+#c=pd.read_excel("C.xlsx")
+#ba=pd.read_excel("b.xlsx")
+#b=c.columns
+#l=ba.columns
 
 uploaded_file = st.file_uploader("Importer le fichier de la revue d'offre", type="xlsx")
 if uploaded_file:
@@ -56,3 +51,72 @@ if uploaded_file:
     df_prop = pd.read_excel(uploaded_file,"PROP")
     df_prop =df_prop.fillna(0)
     df_prop=df_prop[l]
+    
+   
+
+    try:
+        df_c = pd.read_excel(uploaded_file,"C")
+        df_c =df_c.fillna(0)
+        df_c=df_c[l]
+    except:
+        df_c=ba
+
+    df=pd.concat([df_c,df_prop,df_ab,df_dplus,df_dpac,df_dmoins,df_gig])
+    df_revue=df[df["Marché"].isin(options)]
+     
+    df_revue.index=range(len(df_revue))
+    df_revue["Decision finale code dispo"]=df_revue["Decision (Par defaut) : Code dispo"]
+    df_revue["Decision finale Lien appro"]=df_revue["Decision (Par defaut) : Liens d'appro"]
+    for i in range(len(df_revue)): 
+        if df_revue["Decisions CM :Codes dispo"][i] !=0:
+            df_revue["Decision finale code dispo"][i]=df_revue["Decisions CM :Codes dispo"][i]
+           
+        elif df_revue["Decisions RAR :Codes dispo"][i] !=0 :
+            df_revue["Decision finale code dispo"][i]=df_revue["Decisions RAR :Codes dispo"][i]
+            
+
+        elif df_revue["Preconisation CA : Code dispo"][i] !=0 :
+            df_revue["Decision finale code dispo"][i]=df_revue["Preconisation CA : Code dispo"][i]
+           
+
+
+    for i in range(len(df_revue)): 
+        if df_revue["Decisions CM :Liens appro"][i] !=0:
+           
+            df_revue["Decision finale Lien appro"][i]=df_revue["Decisions CM :Liens appro"][i]
+
+        elif df_revue["Decisions RAR :Liens appro"][i] !=0 :
+            
+            df_revue["Decision finale Lien appro"][i]=df_revue["Decisions RAR :Liens appro"][i]
+
+        elif df_revue["Preconisation CA : Liens d'appro"][i] !=0 :
+           
+            df_revue["Decision finale Lien appro"][i]=df_revue["Preconisation CA : Liens d'appro"][i]
+
+    
+
+
+    
+    
+    df_geo=df_revue[["Agence LIB","Article","Decision finale code dispo","Decision finale Lien appro"]]
+    df_geo["Decision finale Lien appro"]=df_geo["Decision finale Lien appro"].apply(str)
+    df_geo["Article"]=df_geo["Article"].apply(str)
+    df_geo["cle"]=df_geo["Agence LIB"]+"-"+df_geo["Article"]
+    df_data=df_data[["cle","Agence","Fournisseur Appro Principal","Article LIB"]]
+    df_geo=df_geo.merge(df_data,how ='left', on ="cle")
+    df_geo=df_geo[["Agence LIB","Agence","Article LIB","Article","Fournisseur Appro Principal","Decision finale code dispo","Decision finale Lien appro"]]
+    st.dataframe(df_geo)
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+    # Write each dataframe to a different worksheet.
+         df_geo.to_excel(writer, sheet_name='Sheet1')
+    
+
+    # Close the Pandas Excel writer and output the Excel file to the buffer
+         writer.save()
+
+         st.download_button(
+           label="Download Excel worksheets",
+           data=buffer,
+           file_name="pandas_multiple.xlsx",
+           mime="application/vnd.ms-excel"
+           )
