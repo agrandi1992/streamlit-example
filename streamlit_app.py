94eefe1233b6b73f9b872e8bf7846eb459724446
agrandi1992 <74638083+agrandi1992@users.noreply.github.com>
2023-01-05 09:56:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,55 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import pandas as pd
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+import io
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+buffer = io.BytesIO()
+c=pd.read_excel("C.xlsx")
+ba=pd.read_excel("b.xlsx")
+b=c.columns
+l=ba.columns
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+uploaded_file = st.file_uploader("Importer le fichier de la revue d'offre", type="xlsx")
+if uploaded_file:
+    options = st.multiselect(
+    'Choisir les marchés ',
+   
+    ["MENUISERIES"	,"QUINCAILLERIE",	"COUVERTURE","PANNEAUX"	,"REVÊTEMENTS"	,"AMENAGEMENT EXTERIEUR",	"AMENAGEMENT INTERIEUR",	"MATERIEL ET OUTILLAGE"	,"BOIS DE CONSTRUCTION"	,"PLAFOND, PLATRERIE, ISOLATION",	"SANITAIRE",	"CHAUFFAGE",	"VRD-TP"	,"ELECTRICITE"	,"PLOMBERIE"	,"PEINTURE / DROGUERIE"	,"PRODUITS BETON INDUSTRIALISES",	"CHARPENTES INDUSTRIALISÉES",	"GROS-ŒUVRE"])
+    st.write('Marchés:', options) 
+
+    
+    df_data = pd.read_excel(uploaded_file,"DATA")
+    df_data =df_data.fillna(0)
+    df_data=df_data[["Agence","Agence LIB","Fournisseur Appro Principal","Article","Article LIB"]]
+    df_data["Article"]=df_data["Article"].apply(str)
+    df_data["cle"]=df_data["Agence LIB"]+"-"+df_data["Article"]
+    df_data=df_data[["cle","Agence","Agence LIB","Fournisseur Appro Principal","Article","Article LIB"]]
+    
+    df_gig = pd.read_excel(uploaded_file,"Gigonité")
+    df_gig=df_gig.fillna(0)
+    
+    df_gig=df_gig[l]
+    
+    df_dmoins = pd.read_excel(uploaded_file,"D-1")
+    df_dmoins =df_dmoins .fillna(0)
+    df_dmoins=df_dmoins[l]
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    df_dpac = pd.read_excel(uploaded_file,"DPAC")
+    df_dpac =df_dpac .fillna(0)
+    df_dpac=df_dpac[l]
+   
+    df_dplus = pd.read_excel(uploaded_file,"D+1")
+    df_dplus =df_dplus .fillna(0)
+    df_dplus=df_dplus[b]
+    df_dplus.columns=l
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    df_ab = pd.read_excel(uploaded_file,"AB")
+    df_ab =df_ab.fillna(0)
+    df_ab=df_ab[l]
+    df_prop = pd.read_excel(uploaded_file,"PROP")
+    df_prop =df_prop.fillna(0)
+    df_prop=df_prop[l]
